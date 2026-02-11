1b98fe61cb691117a647405484f171644b8b4afe
Ashwin <ashwinkrishna815@gmail.com>
2026-02-11 17:45:17 +0530

    updated

--- playwright.config.js
+++ playwright.config.js
@@ -27,7 +27,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
-    ['allure-playwright'],['json']
+    ['allure-playwright'],['json',{ outputFile: 'test-results.json' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
